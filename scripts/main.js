9723eccf66e28f317ba0e829105d03687eaaa778
Veronica <akim60452@gmail.com>
2025-05-22 18:47:04 +0300

    Добавили функцию галереи

--- scripts/main.js
+++ scripts/main.js
@@ -24,4 +24,57 @@ if (contactForm) {
     });
 }
 
+const galleryContainer = document.querySelector(‘.gallery-container’);
 
+const images = document.querySelectorAll(‘.gallery-image’);
+
+const prevButton = document.getElementById(‘prev-btn’);
+
+const nextButton = document.getElementById(‘next-btn’);
+
+let currentImageIndex = 0;
+
+
+function showImage(index) {
+
+images.forEach((image, i) => {
+
+if (i === index) {
+
+image.style.transform = ‘scale(1)’;
+
+} else {
+
+image.style.transform = ‘scale(0)’;
+
+}
+
+});
+
+}
+
+
+function showPreviousImage() {
+
+currentImageIndex = (currentImageIndex — 1 + images.length) % images.length;
+
+showImage(currentImageIndex);
+
+}
+
+
+function showNextImage() {
+
+currentImageIndex = (currentImageIndex + 1) % images.length;
+
+showImage(currentImageIndex);
+
+}
+
+
+prevButton.addEventListener(‘click’, showPreviousImage);
+
+nextButton.addEventListener(‘click’, showNextImage);
+
+
+showImage(currentImageIndex);
